a318822e8df7fc7df842417f19de2460867570e5
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-01-30 23:49:44 +0200

    Add Create Action and View

--- AspNetCoreMVCApp/AspNetCoreMVCWeb/Controllers/CategoryController.cs
+++ AspNetCoreMVCApp/AspNetCoreMVCWeb/Controllers/CategoryController.cs
@@ -15,4 +15,27 @@ public class CategoryController : Controller
         IEnumerable<Category> objCategoryList = _db.Categories;
         return View(objCategoryList);
     }
+
+    //GET
+    public IActionResult Create()
+    {
+        return View();       
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "DisplayOrder cannot be the same with the Name");
+        }
+        if (ModelState.IsValid) { 
+        _db.Categories.Add(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
 }
