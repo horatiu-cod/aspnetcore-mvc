e0ac882bcec4a31ef1b129975a19320b594d3d55
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-01-30 10:12:55 +0200

    Add Startup Service
    Add DependencyInjection class.
    Refactor program.cs.

---
+++ AspNetCoreMVCApp/AspNetCoreMVCWeb/Services/Startup/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreMVCWeb.Services.Startup;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllersWithViews();
+
+        return builder;
+    }
+}
