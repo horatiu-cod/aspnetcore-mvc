13f8775424204b93575bb92e9772b8070b0fd492
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-01-30 12:22:23 +0200

    Add ApplicationDbContext

---
+++ AspNetCoreMVCApp/AspNetCoreMVCLibrary/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreMVCLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreMVCLibrary.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<Category> Categories { get; set; }
+}
